c95fed0e1cc4476f879b945b152984ba3b3779fb
Abhishek-2416 <abhishekalimchandani1624@protonmail.com>
2024-01-12 12:22:30 +0530

    Fourth Commit

--- test/BasicNftTest.t.sol
+++ test/BasicNftTest.t.sol
@@ -9,8 +9,45 @@ contract BasicNftTest is Test {
     BasicNft public basicNft;
     DeployBasicNft public deployer;
 
+    //Addresses
+    address public bob = makeAddr("bob");
+
+    string public constant PUG =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
     function setUp() public {
         deployer = new DeployBasicNft();
         basicNft = deployer.run();
+    }
+
+    function testName() public view {
+        string memory expectedName = "Dogie";
+        string memory actualName = basicNft.name();
+
+        assert(
+            keccak256(abi.encodePacked(actualName)) ==
+                keccak256(abi.encodePacked(expectedName))
+        );
+    }
+
+    function testSymbol() public view {
+        string memory expectedSymbol = "DOG";
+        string memory actualSymbol = basicNft.symbol();
+
+        assert(
+            keccak256(abi.encodePacked(actualSymbol)) ==
+                keccak256(abi.encodePacked(expectedSymbol))
+        );
+    }
+
+    function testCanMintAndHaveBalance() public {
+        vm.prank(bob);
+        basicNft.mintNft(PUG);
+
+        assertEq(basicNft.balanceOf(bob), 1);
+        assertEq(
+            keccak256(abi.encodePacked(PUG)),
+            keccak256(abi.encodePacked(basicNft.tokenURI(0)))
+        );
     }
 }
