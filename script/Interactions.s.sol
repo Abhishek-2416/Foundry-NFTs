07d1471c81830453c9c3183a794d9d4a2cbf80a1
Abhishek-2416 <abhishekalimchandani1624@protonmail.com>
2024-01-15 12:10:00 +0530

    Fifth Commit

---
+++ script/Interactions.s.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+
+contract MintBasicNft is Script {
+    function run() external {
+        //We want it to be working with the latest deployed version for that we use the foundry devops tools
+    }
+}
