94dab03fb7df575c6c22f45e792a2ff9b0e4ec14
Abhishek-2416 <abhishekalimchandani1624@protonmail.com>
2024-03-23 13:31:39 +0530

    Third Commit

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -6,16 +6,18 @@ import {BasicNft} from "../src/BasicNft.sol";
 import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
 
 contract MintBasicNft is Script {
-    string public constant PUG = "https://ipfs.io/ipfs/QmSsYRx3LpDAb1GZQm7zZ1AuHZjfbPkD6J7s9r41xu1mf8?filename=pug.png";
-
-    function mintNftOnContract(address contractAddress) public {
-        vm.startBroadcast();
-        BasicNft(contractAddress).mintNft(PUG);
-        vm.stopBroadcast();
-    }
+    string public constant PUG_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+    uint256 deployerKey;
 
     function run() external {
-        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("BasicNft", block.chainid);
-        mintNftOnContract(mostRecentlyDeployed);
+        address mostRecentlyDeployedBasicNft = DevOpsTools.get_most_recent_deployment("BasicNft", block.chainid);
+        mintNftOnContract(mostRecentlyDeployedBasicNft);
+    }
+
+    function mintNftOnContract(address basicNftAddress) public {
+        vm.startBroadcast();
+        BasicNft(basicNftAddress).mintNft(PUG_URI);
+        vm.stopBroadcast();
     }
 }
